6e1db66222d9bde88791278273bc8a87f79fdb4c
huangyuhong1 <1807092357@qq.com>
2023-11-04 01:33:43 +0800

    123

--- ip_2port_ram/rtl/ip_2port_ram.v
+++ ip_2port_ram/rtl/ip_2port_ram.v
@@ -12,26 +12,25 @@ wire    [7:0] ram_rd_data;
 
 
 
-    ram_wr wr(
-    .clk            (clk        ),
-    .rst_n          (rst_n      ),
-    .ram_wr_en      (ram_wr_en  ),
-    .ram_wr_we      (ram_wr_we  ),
-    .ram_wr_addr    (ram_wr_addr),
-    .ram_wr_data    (ram_wr_data),
-    .rd_flag        (rd_flag    )
-    );
-
-    blk_mem_gen_0 your_instance_name (
-    .clka         (clk),   
-    .ena          (ram_wr_en),     
-    .wea          (ram_wr_we),     
-    .addra        (ram_wr_addr), 
-    .dina         (ram_wr_data),   
-    .clkb         (clk),   
-    .enb          (ram_rd_en ),     
-    .addrb        (ram_rd_addr), 
-    .doutb        (ram_rd_data)  
+ram_wr wr(
+.clk            (clk        ),
+.rst_n          (rst_n      ),
+.ram_wr_en      (ram_wr_en  ),
+.ram_wr_we      (ram_wr_we  ),
+.ram_wr_addr    (ram_wr_addr),
+.ram_wr_data    (ram_wr_data),
+.rd_flag        (rd_flag    )
+);
+blk_mem_gen_0 your_instance_name (
+.clka         (clk),   
+.ena          (ram_wr_en),     
+.wea          (ram_wr_we),     
+.addra        (ram_wr_addr), 
+.dina         (ram_wr_data),   
+.clkb         (clk),   
+.enb          (ram_rd_en ),     
+.addrb        (ram_rd_addr), 
+.doutb        (ram_rd_data)  
 );      
 
     ram_rd rd(
--- ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/xsim_webtalk.tcl
+++ ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Nov  3 19:17:14 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Nov  3 23:53:33 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "6c22736f-19dd-45a4-b305-8d0fc1601230" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "721aff00c82c4fb7bb7b9e360902d6a9" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "22" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "25" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 7 6800H with Radeon Graphics         " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "9664_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1774940689 -regid "" -xml G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "9680_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3399959200 -regid "" -xml G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/FPGA/ip_2port_ram/vivado_prj/ip_2port_ram/ip_2port_ram.sim/sim_1/behav/xsim/xsim.dir/tb_ip_2port_ram_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
